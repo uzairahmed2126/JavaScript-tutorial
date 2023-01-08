f32dec0f456d07853a6803096fc1df9d0c05007b
uzairahmed <uzairahmed21261@gamail.com>
2023-01-08 20:47:32 +0530

    Comments in JavaScript

---
+++ js/4.comments.js
@@ -0,0 +1,22 @@
+// This is normal implementation/single line comment
+// document.write("Hello World!");
+
+/*This is multiline comments
+document.write("Hello World!");
+document.write("Hello World!");
+document.write("Hello World!");
+document.write("Hello World!");
+document.write("Hello World!");
+document.write("Hello World!");
+document.write("Hello World!");
+document.write("Hello World!");
+document.write("Hello World!");
+document.write("Hello World!");
+document.write("Hello World!");
+document.write("Hello World!");
+document.write("Hello World!");
+document.write("Hello World!");
+document.write("Hello World!");
+document.write("Hello World!");
+document.write("Hello World!");
+document.write("Hello World!");*/
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 # [Introduction](./markdown/1.introduction.md)
 # [Implementation](./markdown/2.implementation.md)
 # [HTML Tag in JavaScript](./markdown/3.html%20tag%20in%20JavaScript.md)
+# [Comments](./markdown/4.comments.md)
