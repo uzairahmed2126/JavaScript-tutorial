c1da4256830dc5a1b46d9880e035a0c919fd08dd
uzairahmed <uzairahmed21261@gamail.com>
2023-01-08 19:57:18 +0530

    HTML tag in JavaScript

---
+++ js/3.html tag in JavaScript.js
@@ -0,0 +1,12 @@
+document.write("<h1>hello</h1>");
+document.write("<h2>hello</h2>");
+document.write("<h3>hello</h3>");
+document.write("<h4>hello</h4>");
+document.write("<h5>hello</h5>");
+document.write("<h6>hello</h6>");
+document.write("<hr>hello");
+document.write("<br>hello<hr><br>");
+document.write("<b>hello</b><br>");
+document.write("<i>hello</i><br>");
+document.write("<q>hello</q><br>");
+document.write("<mark>hello</mark>");
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
 # [Introduction](./markdown/1.introduction.md)
 # [Implementation](./markdown/2.implementation.md)
-
+# [HTML Tag in JavaScript](./markdown/3.html%20tag%20in%20JavaScript.md)
