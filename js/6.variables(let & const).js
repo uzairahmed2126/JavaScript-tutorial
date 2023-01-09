5f369bbc6fa6262031350caa926805dda75fdbfa
uzairahmed <uzairahmed21261@gamail.com>
2023-01-09 17:56:09 +0530

    Variable(let & const)

---
+++ js/6.variables(let & const).js
@@ -0,0 +1,20 @@
+let Name= "Hello WORLD ";
+document.write(Name);
+// We can't declare again same variable in let variable
+// let Name= "Hello WORLD";
+// document.write(Name);
+let a = 30;
+a = 40;
+let b = 40;
+let c = a-b;
+
+document.write(c);
+
+const Hello = "this is constant";
+// We can't declare same variable in const variable
+// const Hello = "hey";
+
+// We can't assingn the constant variable if we assign then we find a error in console and it's will not work
+// Hello = "this";
+// console.log(Hello);
+
--- readme.md
+++ readme.md
@@ -2,8 +2,8 @@
 # [Implementation](./markdown/2.implementation.md)
 # [HTML Tag in JavaScript](./markdown/3.html%20tag%20in%20JavaScript.md)
 # [Comments](./markdown/4.comments.md)
-# [Variables(var)](./markdown/5.variable(var).md)
-
+# [Variable(var)](./markdown/5.variable(var).md)
+# [Variables(let&const)](./markdown/6.variables(let%26const).md)
 
 
 
