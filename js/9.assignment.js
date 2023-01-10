0f6641006f09f11a2d3c9fc7dd8181567b94eb20
uzairahmed <uzairahmed21261@gamail.com>
2023-01-10 15:54:47 +0530

    Assignment Operators

---
+++ js/9.assignment.js
@@ -0,0 +1,37 @@
+let a=1;
+let b=2;
+a+=b;
+document.write("Addition is : "+b+'<br>');
+
+let first=3;
+let second=42;
+first-=second;
+document.write("Subtraction is : "+first+'<br>');
+
+let first_=14;
+let second_=2;
+first_/=second_;
+document.write("Division is : "+first_+'<br>');
+
+
+let first_1=12;
+let second_2=3;
+first_1%=second_2;
+document.write("Remainder is : "+first_1+'<br>');
+
+
+let one=14;
+let two=2;
+one*=two;
+document.write("Multiplication is : "+one+'<br>');
+
+let expo=14;
+let expoForsec=2;
+expo**=expoForsec;
+document.write("Exponential is : "+expo+'<br>');
+
+
+let firstName ="Hello";
+let lastName=" World! ";
+firstName+=lastName;
+document.write(firstName);
--- readme.md
+++ readme.md
@@ -5,7 +5,8 @@
 # [Variable(var)](./markdown/5.variable(var).md)
 # [Variables(let&const)](./markdown/6.variables(let%26const).md)
 # [Data Types](./markdown/7.data%20types.md)
-# [Arithmetic Opratations](./markdown/8.arithmetic.md)
+# [Arithmetic Operatations](./markdown/8.arithmetic.md)
+# [Assignment Operators](./markdown/9.assignment.md)
 
 
 
