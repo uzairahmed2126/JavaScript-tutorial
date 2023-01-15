1d2b0e4cd403edba433ef13ba5c6c01da1d9cfe3
uzairahmed <uzairahmed21261@gamail.com>
2023-01-15 10:26:38 +0530

    Example add

--- js/15.if else if statement.js
+++ js/15.if else if statement.js
@@ -17,3 +17,16 @@ if(NighttoMorning == 12+"am"){
 }else{
     console.log("maybe Good After Noon or Good Night")
 }
+
+const per =100;
+if (per >= 80 && per <=100){
+    console.log("merit")
+}else if (per >= 60 && per <80){
+    console.log("i")
+}else if(per >=45 && per < 60){
+    console.log("ii")
+}else if(per >=33 && per < 45){
+    console.log("iii")
+}else if(per < 33){
+    console.log("you are fail")
+}else console.log("please enter valid percent")
