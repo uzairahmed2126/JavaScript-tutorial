f54a691b22320190bd432e18b4465caf19587253
uzairahmed <uzairahmed21261@gamail.com>
2023-01-25 22:20:34 +0530

    global and local variable

--- js/24.global & local variable.js
+++ js/24.global & local variable.js
@@ -4,6 +4,10 @@ function b(hello){
     // it's a local variable and we can't access local variable outof scope
     let c = 22;
     console.log(c)
+    console.log(a)
     return hello;
+
 }
 console.log(b("uzair"));
+console.log(a)
+
