ce4aee09680d7ccef791ddfb7e2ad630cb2007ba
uzairahmed <uzairahmed21261@gamail.com>
2023-02-05 20:05:24 +0530

    forEach

---
+++ js/49.forEach.js
@@ -0,0 +1,11 @@
+let a =["string ","string ","string ","string ","string "]
+a.forEach(function(value,index){
+    console.log(value,index)
+
+});
+
+let num = [12,13,14,15,16];
+num.forEach(int);
+function int(value){
+console.log(value)
+}
