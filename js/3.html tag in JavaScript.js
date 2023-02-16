07de34390cfc281e276e237969722e2c926ae605
uzairahmed <uzairahmed21261@gamail.com>
2023-02-16 22:29:01 +0530

    modify some lines

--- js/3.html tag in JavaScript.js
+++ js/3.html tag in JavaScript.js
@@ -10,3 +10,12 @@ document.write("<b>hello</b><br>");
 document.write("<i>hello</i><br>");
 document.write("<q>hello</q><br>");
 document.write("<mark>hello</mark>");
+
+document.write("<select name='select' id='name'>");
+document.write('<option value="Select">___Select_our_courses__</option>')
+document.write('<option value="CSS">CSS</option>')
+document.write('<option value="Js">JavaScript</option>')
+document.write('<option value="Python">Paython</option>')
+document.write('<option value="Data Base">Data Base</option>')
+document.write('<option value="English">English</option>')
+document.write('</select>');
--- js/5.variable(var).js
+++ js/5.variable(var).js
@@ -8,6 +8,6 @@ var forth4name = 40;
 var fifth = 20;
 var seven7 = FirstName+Second_name+thirdName+forth4name+fifth;
 document.write(seven7);
-// wrong to write variable
+// wrong way to write variable
 // var 32name =1;
 // var name-1 =2;
