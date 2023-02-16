4b85e13ba60ec417e490ca37ed2ca821ed17c969
uzairahmed <uzairahmed21261@gamail.com>
2023-02-17 00:19:25 +0530

    modify some lines

--- js/31.nested loop.js
+++ js/31.nested loop.js
@@ -1,12 +1,22 @@
-for(let a=100; a>1; a=a-10){
-    for(let b=a;b<=a+10;b++)
-        document.write(b+" ")
-        document.write("<br>")    
-}
+// for(let a=100; a>1; a=a-10){
+//     for(let b=a;b<=a+10;b++)
+//         document.write(b+" ")
+//         document.write("<br>")    
+// }
 
-let check =0;
-for (let i =1; i<= 7; i++)
-{
-    check += i-1;
-    console.log(check);
+// let check =0;
+// for (let i =1; i<= 7; i++)
+// {
+//     check += i-1;
+//     console.log(check);
+// }
+
+for(let out=1;out<100;out+=10){
+    document.write("<br>")        
+    for(inner=out;inner<out+10;inner++){
+        document.write(inner+ ' ')
+
+}
+// document.write("<br>")
+
 }
