781d0251cabaec9cfef76ced006a02abd28d5835
uzairahmed <uzairahmed21261@gamail.com>
2023-02-18 14:40:05 +0530

    more paractice on slice and splice

--- js/41.slice and splice.js
+++ js/41.slice and splice.js
@@ -1,9 +1,21 @@
-let a = ["uzair","adnan","saleh","hasan"]
-document.write(a+"<br>")
-let b = a.slice(0,2)
-document.write(b)
+// let a = ["uzair","adnan","saleh","hasan"]
+// document.write(a+"<br>")
+// let b = a.slice(0,2)
+// document.write(b)
 
-let x=["uzair","adnan","saleh","hasan"]
-document.write(x+"<br>")
-x.splice(1,0,"ahmed");
-document.write(x);
+// let x=["uzair","adnan","saleh","hasan"]
+// document.write(x+"<br>")
+// x.splice(1,0,"ahmed");
+// document.write(x);
+// It's make a new array
+let s= ["uzair","rehan","ibrahim","adnan","umer"]
+document.write("1)",s+"<br>")
+let v = s.slice(0,3)
+document.write(v+"<br><br>")
+
+// It's change a existing array
+let sp = ["uzair","rehan","ibrahim","adnan","umer"];
+document.write("2)"+sp+"<br>")
+sp.splice(1,0,"ahmed")
+document.write(sp+"<br>")
+document.write(sp.join(" "))
