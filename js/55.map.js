d805ab3753413200f9d7f3be1c1028fb14011e37
uzairahmed <uzairahmed21261@gamail.com>
2023-02-22 12:36:27 +0530

    some interction add

--- js/55.map.js
+++ js/55.map.js
@@ -1,15 +1,23 @@
-let arr = [{fname :"uzair",lastname : "ahmed"} ];
-b=arr.map(test);
+let arr = [{ fname: 'uzair', lastname: 'ahmed' }]
+let b = arr.map(test)
 document.write(b)
-function test(x){
-    console.log(x.fname);
+function test(x) {
+  console.log(x.fname)
 }
 
-let uzz = [12.3,2344,323,2,4]
-con=uzz.map(mult);
-console.log(con);
-function mult(f){
-    return f*10;
+let uzz = [12.3, 2344, 323, 2, 4]
+let con = uzz.map(mult)
+console.log(con)
+function mult(f) {
+  return f * 10
 }
 
+let arry = [
+  {first: 'hello', second: 'world'},
+  {first : 'include', second : 'stdio.h'},
+  {first : 'include', second : 'conio.h'}];
 
+let d = arry.map(conct)
+function conct(y) {
+return  console.log(y.first)
+}
