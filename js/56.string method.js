93f155291c430b35d96cc87091c1e98eb3c90d5a
uzairahmed <uzairahmed21261@gamail.com>
2023-02-23 15:39:51 +0530

    String Methods

--- js/56.string method.js
+++ js/56.string method.js
@@ -1,23 +1,36 @@
 let a = "hello world";
 console.log(a.length);
 
-let Upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+let Upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 console.log(Upper.toLowerCase()); 
 
-let lower = "abcdefghijklmnopqrstuvwxyz"
+let lower = "abcdefghijklmnopqrstuvwxyz";
 console.log(lower.toUpperCase());
 
 let inc = lower.includes('u');
-console.log(inc)
+console.log(inc);
 
-let stwith = lower.startsWith('a')
+let stwith = lower.startsWith('a');
 let enwith = lower.endsWith('z');
-console.log(stwith,enwith)
+console.log(stwith,enwith);
 
-let str = "JavaScript is a great is language in the world "
+let str = "          JavaScript is a       great language in the world JavaScript is good";
 srch = str.search("is");
 console.log(srch);
 
 let mat = str.match(/is/g); 
-console.log(mat)
+console.log(mat);
+
+let indx = str.indexOf("is");
+console.log(indx);
+let lindx = str.lastIndexOf("in");
+console.log(lindx);
+
+let replc =str.replace(/JavaScript/g,"HTML");
+console.log(replc);
+
+let trm = str.trim();
+alert(str+" Before triming");
+alert(trm+" After triming");
+
 
