6030fd348966aac9c7e4ab2e44e7cb6f59c12918
uzairahmed <uzairahmed21261@gamail.com>
2023-03-03 23:57:41 +0530

    DOM CSS Styling

--- js/65.DOM css styling method.js
+++ js/65.DOM css styling method.js
@@ -2,10 +2,21 @@ let element;
 // element = document.querySelector("#hireme").style.fontStyle="italic";
 // console.log(element);
 
+
 function fun(){
     console.log(document.querySelector("#hireme").style.color="green");
 }
 function fun1(){
+    console.log(document.querySelector("#hireme").style.backgroundColor="pink");
     console.log(document.querySelector("#hireme").style.color="red");
+    alert(document.querySelector("#hireme").style.color="red");
+}
+// If any css property have a hyphen so when we want to write in querySelector then we write in the form of camelCase.
+function wheel(){
+    console.log(document.querySelector("p").style.backgroundColor="aqua");
+    console.log(document.querySelector("p").style.color="black");
+    
 }
 
+element=document.querySelector("#hireme").className = "c_hire abc";
+// console.log(element)
