932cdf3dbeff5dda171e2913d6c5edea7616af77
uzairahmed <uzairahmed21261@gamail.com>
2023-03-08 02:13:23 +0530

    Some itrections add

--- js/23.functions with return value.js
+++ js/23.functions with return value.js
@@ -2,9 +2,22 @@ function fn(a,b){
     return b-a;
 }
 console.log(fn(1,7));
+
+// It's a function scope
 function fun(parameter1,parameter2){
     hola = parameter1 + " " +parameter2;
     return hola;
     
 }
 console.log(fun("uzair","ahmed"));
+
+
+// It's a block of code 
+{
+    // can't access this value cause this variable declaration in the block of code and when we have to use var variable so it will be run cause we can assign the var variable any where out of block and between the block.
+    let a = 10;
+}
+// JavaScript Hoisting refers to the process whereby the interpreter appears to move the declaration of functions, variables or classes to the top of their scope, prior to execution of the code
+console.log(a);
+
+// A callback function is a function passed into another function as an argument, which is then invoked inside the outer function to complete some kind of routine or action
