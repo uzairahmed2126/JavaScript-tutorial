0c14ed2dc208fe3e202bab48edbdf9e811c20a4e
uzairahmed <uzairahmed21261@gamail.com>
2023-03-08 14:44:03 +0530

    Travesal Method

--- js/68.parentElement parentNode.js
+++ js/68.parentElement parentNode.js
@@ -5,5 +5,9 @@ console.log(a);
 // It's throw a node of html that is document and when we check the node of document it's throw a error
 let b = document.querySelector("html").parentNode;
 console.log(b);
-
+document.getElementById("hireme").parentElement.style.background="lightblack"
+let c =document.querySelector("#hireme").parentElement
+console.log(c);
+document.querySelector("div").parentElement.style.color="green"
+document.querySelector("div").style.borderRadius="890px"
 
